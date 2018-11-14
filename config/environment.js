065f75b1a815e070d529df9c8ba3901bf5aacfa5
Michael Lange <dingoeatingfuzz@gmail.com>
2018-11-14 14:10:01 -0800

    Tweak env for different timezone

--- config/environment.js
+++ config/environment.js
@@ -4,8 +4,8 @@ module.exports = function(environment) {
   let ENV = {
     'ember-resolver': {
       features: {
-        EMBER_RESOLVER_MODULE_UNIFICATION: true
-      }
+        EMBER_RESOLVER_MODULE_UNIFICATION: true,
+      },
     },
     modulePrefix: 'emberfest',
     environment,
@@ -15,21 +15,21 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-        'ember-module-unification': true
+        'ember-module-unification': true,
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
+        Date: false,
+      },
     },
 
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-      UTC_OFFSET: '+02:00',
+      UTC_OFFSET: '-08:00',
       shouldForceDayOne: false,
-      shouldUpdateTime: true
-    }
+      shouldUpdateTime: true,
+    },
   };
 
   if (environment === 'development') {
